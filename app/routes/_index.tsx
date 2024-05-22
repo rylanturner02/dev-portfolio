85df85dccb17b7850f382ad0b73262ca82129d44
Rylan Turner <62808022+rylanturner02@users.noreply.github.com>
2024-05-22 17:14:35 -0500

    Initialized Remix project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,14 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Rylan Turner Dev Portfolio" }
   ];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to my dev portfolio!</h1>
       <ul>
         <li>
           <a
