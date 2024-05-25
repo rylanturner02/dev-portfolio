d6e2c3806e29805f653917b38fff4308152c6e12
Rylan Turner <62808022+rylanturner02@users.noreply.github.com>
2024-05-25 15:40:08 -0500

    Added GitHub button; changed body font.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -28,16 +28,18 @@ function Card({ repoName, description, tags } : CardProps) {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "Averia Serif Libre, serif", lineHeight: "1.8", padding: "20px" }}>
-      <header className="fixed top-0 left-0 right-0 z-50">
-        <div className="navbar bg-primary">
+    <div style={{ fontFamily: "Zilla Slab, serif", lineHeight: "1.8", padding: "20px" }}>
+      <header className="fixed bg-base-100 top-0 left-0 right-0 z-50">
+        <div className="navbar">
           <div className="flex-1">
-            <a className="btn btn-ghost text-xl">Rylan Turner&apos;s Portfolio</a>
+            <a style={{fontFamily: "Caprasimo, serif"}} className="btn btn-ghost sm:text-xl md:text-3xl tracking-wide">Rylan Turner&apos;s Portfolio</a>
           </div>
           <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-            </button>
+            <a href="https://www.github.com/rylanturner02" className="btn btn-square btn-ghost">
+            <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
+              <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+            </svg>
+            </a>
           </div>
         </div>
       </header>
